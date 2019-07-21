97e19f95321736a37756709789e803f1b906e6a1
jamilya824 <jama.98.jk@gmail.com>
2019-07-22 00:36:25 +0600

    Initial commit

---
+++ mytun.py
@@ -0,0 +1,10 @@
+from pytun import TunTapDevice
+
+tun = TunTapDevice(name='mytun')
+tun.addr = '192.168.137.10'
+tun.dstaddr = '192.168.137.1'
+tun.netmask = '255.255.255.0'
+tun.mtu = 1500
+tun.persist(True)
+buf = tun.read(tun.mtu)
+tun.write(buf)
