7c5149fdf649fa38b2a01119dd7c2621f72eb1ec
jamilya824 <jama.98.jk@gmail.com>
2019-07-23 22:55:47 +0600

    netmask corrected

--- mytun.py
+++ mytun.py
@@ -3,8 +3,9 @@ from pytun import TunTapDevice
 tun = TunTapDevice(name='mytun')
 tun.addr = '192.168.137.10'
 tun.dstaddr = '192.168.137.1'
-tun.netmask = '255.255.255.255'
+tun.netmask = '255.255.255.240'
 tun.mtu = 1500
 tun.persist(True)
+tun.up()
 buf = tun.read(tun.mtu)
 tun.write(buf)
